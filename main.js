14d6a87f40bb328c4b170643fad60043435fdef5
Richard Marquez <rmarquez14@winona.edu>
2014-11-20 16:55:23 -0600

    portfolio images

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  portfolioInit();
+}
+
+function portfolioInit() {
+  var list = document.getElementById("portfolio").getElementsByTagName("li");
+
+  for(var i = 0; i < list.length; i++) {
+    list[i].onclick = function() { alert(this.firstChild.textContent); }
+  }
+}
