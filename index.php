36f2eb2030edc8658abb9851442588a7f3f917b4
Richard Marquez <rmarquez14@winona.edu>
2014-11-30 04:52:06 -0600

    dynamic portfolio content

--- index.php
+++ index.php
@@ -10,79 +10,71 @@
     <script src="main.js"></script>
   </head>
   <body>
+    <!--<div id="shade" onclick="$(this).toggle();"></div>-->
+
     <header>
-      <div class="container">
-        <h1 id="title">Richard Marquez</h1>
-        <nav>
-          <ul>
-            <li><a href="https://github.com/richard92m">Code</a></li>
-            <li><a href="http://richard92m.svbtle.com/">Writings</a></li>
-            <li><a href="http://careers.stackoverflow.com/richard92m">R&eacute;sum&eacute;</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-        <div class="clear"></div>
-      </div>
+      <h1 id="title">Richard Marquez</h1>
+      <nav>
+        <ul>
+          <li><a href="https://github.com/richard92m">Code</a></li>
+          <li><a href="http://richard92m.svbtle.com/">Writings</a></li>
+          <li><a href="http://careers.stackoverflow.com/richard92m">R&eacute;sum&eacute;</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="clear"></div>
     </header>
 
     <section id="writings">
-      <div class="container">
-        <h2>Writings</h2>
-      </div>
+      <h2>Writings</h2>
     </section>
 
     <section id="portfolio">
-      <div class="container">
-        <h2>Portfolio</h2>
-        <ul>
-          <?php
-            $portfolioDir = 'portfolio/';
-            $portfolioImageDir = $portfolioDir . 'images/';
-            $files = scandir($portfolioImageDir);
+      <h2>Portfolio</h2>
+      <ul id="portfolioList">
+        <?php
+          $portfolioDir = 'portfolio/';
+          $portfolioImageDir = $portfolioDir . 'images/';
+          $files = scandir($portfolioImageDir);
 
-            foreach ($files as $file) {
-              // Do not include hidden files
-              if (preg_match('/^\./', $file) == 1) {
-                continue;
-              }
-
-              $info = pathinfo($file);
-              $cleanName = basename($file, '.' . $info['extension']);
-
-              echo '<li>';
-
-              echo '<h3>' . $cleanName . '</h3>';
-              echo '<img alt="' . $cleanName . '" src="' . $portfolioImageDir . $file . '" />';
-
-              echo '</li>';
+          foreach ($files as $file) {
+            // Do not include hidden files
+            if (preg_match('/^\./', $file) == 1) {
+              continue;
             }
-            
-          ?>
-        </ul>
-      </div>
+
+            $info = pathinfo($file);
+            $cleanName = basename($file, '.' . $info['extension']);
+
+            echo '<li>';
+
+            echo '<h3>' . $cleanName . '</h3>';
+            echo '<img alt="' . $cleanName . '" src="' . $portfolioImageDir . $file . '" />';
+
+            echo '</li>';
+          }
+          
+        ?>
+      </ul>
     </section>
 
     <section id="feed">
-      <div class="container">
-        <h2>Feed</h2>
-      </div>
+      <h2>Feed</h2>
     </section>
 
     <footer>
-      <div class="container">
-        <img id="gravatar" src="http://www.gravatar.com/avatar/fc42561857fe799ddb21988f73675681.png" alt="Gravatar" />
-        <ul id="social-media">
-          <li><a href="https://www.facebook.com/richard92m"><img alt="Facebook" src="resources/social_media/facebook.png" /></a></li>
-          <li><a href="http://www.linkedin.com/pub/richard-marquez/84/927/733/"><img alt="LinkedIn" src="resources/social_media/linkedin.png" /></a></li>
-          <li><a href="https://github.com/richard92m"><img alt="Skype" src="resources/social_media/skype.png" /></a></li>
-        </ul>
-        <ul id="contact">
-          <li><h3>Email:</h3> <a href="mailto:richard92m@me.com">richard92m@me.com</a></li>
-          <li><h3>Skype:</h3> richard92m</li>
-          <li><h3>Phone:</h3> 507.450.2426</li>
-          <li>Winona, MN - U.S.A.</li>
-        </ul>
-      </div>
+      <img id="gravatar" src="http://www.gravatar.com/avatar/fc42561857fe799ddb21988f73675681.png" alt="Gravatar" />
+      <ul id="social-media">
+        <li><a href="https://www.facebook.com/richard92m"><img alt="Facebook" src="resources/social_media/facebook.png" /></a></li>
+        <li><a href="http://www.linkedin.com/pub/richard-marquez/84/927/733/"><img alt="LinkedIn" src="resources/social_media/linkedin.png" /></a></li>
+        <li><a href="https://github.com/richard92m"><img alt="Skype" src="resources/social_media/skype.png" /></a></li>
+      </ul>
+      <ul id="contact">
+        <li><h3>Email:</h3> <a href="mailto:richard92m@me.com">richard92m@me.com</a></li>
+        <li><h3>Skype:</h3> richard92m</li>
+        <li><h3>Phone:</h3> 507.450.2426</li>
+        <li>Winona, MN - U.S.A.</li>
+      </ul>
       <div class="clear"></div>
     </footer>
   </body>
--- main.js
+++ main.js
@@ -3,9 +3,49 @@ window.onload = function() {
 }
 
 function portfolioInit() {
-  /*var list = document.getElementById("portfolio").getElementsByTagName("li");
+  // Set onclick() for each work
+  $('#portfolioList').children().each(function() {
 
-  for(var i = 0; i < list.length; i++) {
-    list[i].onclick = function() { alert(this.firstChild.textContent); }
-  }*/
+    $(this).click(function() {
+      showWork($(this).children('h3').html(), $(this).children('img').attr('src'));
+    });
+
+  });
+}
+
+function showWork(name, imgSrc) {
+  var shade = $('<div id="shade"></div>');
+  shade.click(function() {
+    removeWork();
+  });
+  $('body').append(shade);
+
+  var workContainer = $('<div id="workContainer"></div>');
+  $('body').append(workContainer);
+
+  var header = $('<h1>' + name + '</h1>');
+  $('#workContainer').append(header);
+
+  var image = $('<img alt="' + name + '" src="' + imgSrc + '" />');
+  $('#workContainer').append(image);
+
+  $.get('portfolio/text/' + name, function(data) {
+
+    var desc = $('<div id="description">' + data + '</div>');
+    $('#workContainer').append(desc);
+
+
+    var close = $('<div id="close">Close</div>');
+    close.click(function() {
+      removeWork();
+    });
+
+    $('#workContainer').append(close);
+  });
+
+}
+
+function removeWork() {
+    $('#shade').remove();
+    $('#workContainer').remove();
 }
