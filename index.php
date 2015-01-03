ac4759b53c384cce1bb7a3618172cd83cf4234c0
Richard Marquez <rmarquez14@winona.edu>
2015-01-02 19:00:36 -0600

    new design

--- index.php
+++ index.php
@@ -4,78 +4,183 @@
     <meta charset="utf-8" />
     <title>Richard Marquez</title>
     <link rel="stylesheet" type="text/css" href="main.css" />
+    <link rel="stylesheet" type="text/css" href="small-screen.css" />
+    <link rel="stylesheet" type="text/css" href="large-screen.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
     <link rel="icon" href="resources/favicon.png" />
     <link rel="apple-touch-icon-precomposed" href="resources/favicon.png" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="main.js"></script>
   </head>
   <body>
-    <!--<div id="shade" onclick="$(this).toggle();"></div>-->
-
     <header>
-      <h1 id="title">Richard Marquez</h1>
-      <nav>
-        <ul>
-          <li><a href="https://github.com/richard92m">Code</a></li>
-          <li><a href="http://richard92m.svbtle.com/">Writings</a></li>
-          <li><a href="http://careers.stackoverflow.com/richard92m">R&eacute;sum&eacute;</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-      <div class="clear"></div>
+      <div class="container">
+        <h1 id="title">Richard Marquez</h1>
+        <nav>
+          <ul>
+            <li><a href="https://github.com/richard92m">Code</a></li>
+            <li><a href="http://scriptogr.am/richard92m/archive">Writings</a></li>
+            <li><a href="http://careers.stackoverflow.com/richard92m">R&eacute;sum&eacute;</a></li>
+            <li><a class="button" href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+        <div class="clear"></div>
+      </div>
     </header>
 
-    <section id="writings">
-      <h2>Writings</h2>
+    <section class="about purple">
+      <div class="container">
+        <img class="gravatar" src="resources/richardmarquez.jpg" alt="Gravatar" />
+
+        <ul class="social-media">
+          <li><a href="https://www.facebook.com/richard92m"><img alt="Facebook" src="resources/social_media/facebook.png" /></a></li>
+          <li><a href="http://www.linkedin.com/pub/richard-marquez/84/927/733/"><img alt="LinkedIn" src="resources/social_media/linkedin.png" /></a></li>
+          <li><a href="https://github.com/richard92m"><img alt="GitHub" src="resources/social_media/github.png" /></a></li>
+        </ul>
+
+        <div class="info">
+          <p>
+            <img src="resources/email.png" /><a href="mailto:richard92m@me.com">richard92m@me.com</a><br />
+            <img src="resources/location.png" />Winona, MN
+          </p>
+      </div>
+
+      <p class="blurb">
+        aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+        aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+        aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+        aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+        aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+      </p>
     </section>
 
-    <section id="portfolio">
-      <h2>Portfolio</h2>
-      <ul id="portfolioList">
-        <?php
-          $portfolioDir = 'portfolio/';
-          $portfolioImageDir = $portfolioDir . 'images/';
-          $files = scandir($portfolioImageDir);
+    <section class="work green">
+      <div class="container">
+        <div class="pic">
+          <img src="resources/portfolio/miller.png" alt="Miller Ingenuity" />
+        </div>
 
-          foreach ($files as $file) {
-            // Do not include hidden files
-            if (preg_match('/^\./', $file) == 1) {
-              continue;
-            }
+        <h2>Miller Ingenuity</h2>
 
-            $info = pathinfo($file);
-            $cleanName = basename($file, '.' . $info['extension']);
+        <ul class="skills">
+          <li>Design</li>
+          <li>Web</li>
+          <li>Systems</li>
+        </ul>
 
-            echo '<li>';
-
-            echo '<h3>' . $cleanName . '</h3>';
-            echo '<img alt="' . $cleanName . '" src="' . $portfolioImageDir . $file . '" />';
-
-            echo '</li>';
-          }
-          
-        ?>
-      </ul>
+        <p class="blurb">
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+        </p>
+        <div class="clear"></div>
+      </div>
     </section>
 
-    <section id="feed">
-      <h2>Feed</h2>
+    <section class="work aqua">
+      <div class="container">
+        <div class="pic">
+          <img src="resources/portfolio/upac.png" alt="Winona State UPAC" />
+        </div>
+
+        <h2>Winona State UPAC</h2>
+
+        <ul class="skills">
+          <li>Programming</li>
+          <li>iOS</li>
+          <li>Backend</li>
+          <li>Design</li>
+        </ul>
+
+        <p class="blurb">
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+        </p>
+
+        <a href="https://itunes.apple.com/us/app/winona-state-upac/id944827063" target="_blank">
+        <img class="badge" src="resources/app_store_badge.png" alt="See on App Store" />
+        </a>
+
+        <div class="clear"></div>
+      </div>
+    </section>
+
+    <section class="work blue">
+      <div class="container">
+        <div class="pic">
+          <br/><br/><br/>
+          <img src="resources/portfolio/freac.png" alt="fre.ac" />
+        </div>
+
+        <h2>fre.ac</h2>
+
+        <ul class="skills">
+          <li>Programming</li>
+          <li>iOS</li>
+          <li>Game</li>
+          <li>Art</li>
+        </ul>
+
+        <p class="blurb">
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+          aslflsdjflaj dflajdklsfjaklds fjaklds fkla kldf kla jdfklj aklsdj fklaj dklfj sdf
+        </p>
+
+        <a href="https://github.com/richard92m/three.bar" target="_blank">
+        <img class="badge" src="resources/github_badge.png" alt="See on GitHub" />
+        </a>
+
+        <div class="clear"></div>
+      </div>
+    </section>
+
+
+    <section id="contact" class="contact gray">
+      <div class="container">
+        <h2>Get in Touch</h2>
+        <h3><img src="resources/available.png" alt="Available" /> Available Now</h3>
+
+        <div class="info">
+          <img class="gravatar" src="resources/richardmarquez.jpg" alt="Gravatar" />
+
+          <ul class="social-media">
+            <li><a href="https://www.facebook.com/richard92m"><img alt="Facebook" src="resources/social_media/facebook.png" /></a></li>
+            <li><a href="http://www.linkedin.com/pub/richard-marquez/84/927/733/"><img alt="LinkedIn" src="resources/social_media/linkedin.png" /></a></li>
+            <li><a href="https://github.com/richard92m"><img alt="GitHub" src="resources/social_media/github.png" /></a></li>
+          </ul>
+
+          <ul class="blurb">
+            <li><h4>Email:</h4> <a href="mailto:richard92m@me.com">richard92m@me.com</a></li>
+            <li><h4>Skype:</h4> richard92m</li>
+            <li><h4>Phone:</h4> 507.450.2426</li>
+            <li>Winona, MN - U.S.A.</li>
+          </ul>
+        </div>
+
+        <form>
+          <input type="text" value="Name" />
+          <input type="text" value="Email" />
+          <textarea>Message</textarea>
+          <input class="button" type="submit" value="Submit" />
+        </form>
+
+        <div class="clear"></div>
+      </div>
     </section>
 
     <footer>
-      <img id="gravatar" src="http://www.gravatar.com/avatar/fc42561857fe799ddb21988f73675681.png" alt="Gravatar" />
-      <ul id="social-media">
-        <li><a href="https://www.facebook.com/richard92m"><img alt="Facebook" src="resources/social_media/facebook.png" /></a></li>
-        <li><a href="http://www.linkedin.com/pub/richard-marquez/84/927/733/"><img alt="LinkedIn" src="resources/social_media/linkedin.png" /></a></li>
-        <li><a href="https://github.com/richard92m"><img alt="Skype" src="resources/social_media/skype.png" /></a></li>
-      </ul>
-      <ul id="contact">
-        <li><h3>Email:</h3> <a href="mailto:richard92m@me.com">richard92m@me.com</a></li>
-        <li><h3>Skype:</h3> richard92m</li>
-        <li><h3>Phone:</h3> 507.450.2426</li>
-        <li>Winona, MN - U.S.A.</li>
-      </ul>
-      <div class="clear"></div>
+      <p>
+        Copyright <em>Richard Marquez</em> 2015 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="http://linode.com">Linode</a> | <a href="http://lighttpd.net">Lighttpd</a>
+      </p>
     </footer>
   </body>
 </html>
