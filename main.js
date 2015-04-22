b66ab25ef6c9c432d720e01b022222f392a422d8
Richard Marquez <rmarquez14@winona.edu>
2015-04-22 01:22:04 -0500

    removed contact form, mobile menu

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 window.onload = function() {
   addLinkTargets('_blank');
-  menuInit();
-  contactFormInit();
 }
 
 function addLinkTargets(target) {
@@ -12,32 +10,3 @@ function addLinkTargets(target) {
     }
   });
 }
-
-function menuInit() {
-  $('#menu-button').click(function() {
-    $('nav').toggle();
-  });
-}
-
-function contactFormInit() {
-  inputInit($('#contact-name'), 'name');
-  inputInit($('#contact-email'), 'email');
-  inputInit($('#contact-message'), 'message');
-}
-
-function inputInit(input, value) {
-  $(input).focusin(function() {
-    if ($(this).val() == value) {
-      $(this).val('');
-      $(this).css('color', '#000');
-    }
-  });
-
-  $(input).focusout(function() {
-    if ($(this).val() == '') {
-      $(this).val(value);
-      $(this).css('color', '#666');
-    }
-  });
-}
-
