9621ac0b280f5ff09473095d0d1759cf2fec7fd1
Richard Marquez <rmarquez14@winona.edu>
2015-07-07 19:43:22 -0500

    wopr ie !important test

--- public/js/wopr.js
+++ public/js/wopr.js
@@ -7,8 +7,8 @@ function play() {
     var msie = ua.indexOf("MSIE ");
 
     if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer, return version number
-        $('#wopr').css('color', '#f00');
-    
+        $('#wopr').css('color', '#f00 !important');
+
     displayGreetings();
 }
 
