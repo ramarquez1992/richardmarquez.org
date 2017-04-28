28c4762fc2913de249aa4011c9ffd0933636b412
Richard Marquez <richard92m@me.com>
2017-04-27 21:58:01 -0500

    block and avatar animations set

--- js/main.js
+++ js/main.js
@@ -4,17 +4,28 @@ $(document).ready(function() {
 
 function initColorBlocks() {
   $('.square').each(function() {
-    $(this).css('background-color', getRandomColor());
-    resizeColorBlocks();
+    var colorBlock = $(this);
+    var timerId;
+
+    // Assign initial random color
+    colorBlock.css('background-color', getRandomColor());
+    // Set block height to 1/5 viewport y
+    colorBlock.css('height', $(window).height() / 5);
+
+    // Continuously change block color when hovered over
+    colorBlock.mouseover(function() {
+      timerId = setInterval(function() {
+        colorBlock.css('background-color', getRandomColor());
+      }, 100);
+    });
+
+    colorBlock.mouseleave(function() {
+      clearInterval(timerId);
+    });
   });
 }
 
-function resizeColorBlocks() {
-  $('.square').each(function() {
-    $(this).css('height', $(window).height() / 5);
-  });
-}
-
+// Get random 6-digit hex color e.g. "#a82df1"
 function getRandomColor() {
   var letters = '0123456789ABCDEF'.split('');
   var color = '#';
