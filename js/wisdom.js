ef46d1036056e81507054d5a0c58e476d6bec729
Richard Marquez <richard92m@me.com>
2017-05-26 19:25:40 -0400

    quote randomized

---
+++ js/wisdom.js
@@ -0,0 +1,15 @@
+var quotes;
+
+$( document ).ready( function () {
+    // Remember this is called asynchronously
+    $.getJSON( './js/quotes.json', function ( data ) {
+        quotes = data.quotes;
+        initQuote();
+    } );
+
+} );
+
+function initQuote() {
+    var randomIndex = Math.floor((Math.random() * quotes.length));
+    $('#quote').html(quotes[randomIndex]);
+}
