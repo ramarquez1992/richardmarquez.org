b64410d3aeea720b9f2e1bce86a4b9b966219cf9
Richard Marquez <richard92m@me.com>
2017-05-27 08:55:03 -0400

    disabled extra [broken] randomization

--- js/wisdom.js
+++ js/wisdom.js
@@ -14,17 +14,16 @@ function initQuote(quotes) {
   quotesList = quotes;
 
   setInterval(function () {
-    var randomIndex = Math.floor((Math.random() * quotesList.length));
-
     $('main').fadeOut(500, function () {
+      var randomIndex = Math.floor((Math.random() * quotesList.length));
       $('#quote').html(quotesList[randomIndex]);
 
       $('main').fadeIn(500, function () {
-        queue.push(quotesList.splice(randomIndex));
-
-        if (queue.length > 5) {
-          quotesList.push(queue.shift());
-        }
+        // queue.push(quotesList.splice(randomIndex));
+        //
+        // if (queue.length > 5) {
+        //   quotesList.push(queue.shift());
+        // }
       });
     });
   }, 3000);
