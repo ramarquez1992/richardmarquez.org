7aa3eb92625e5ea6afa35adb41ce30c3016a9de4
Richard Marquez <richard92m@me.com>
2017-05-27 08:49:56 -0400

    es5-ified

--- js/wisdom.js
+++ js/wisdom.js
@@ -1,10 +1,10 @@
-const quotes;
-let queue = []; // use push() and shift() to get a queue
-let quotesList = [];
+var quotes;
+var queue = []; // use push() and shift() to get a queue
+var quotesList = [];
 
-$(document).ready(() => {
+$(document).ready(function () {
   // Remember this is called asynchronously
-  $.getJSON('./js/quotes.json', (data) => {
+  $.getJSON('./js/quotes.json', function (data)  {
     quotes = data.quotes;
     initQuote(quotes);
   });
@@ -12,12 +12,16 @@ $(document).ready(() => {
 
 function initQuote(quotes) {
   quotesList = quotes;
-  setInterval(() => {
-    let randomIndex = Math.floor((Math.random() * quotesList.length));
-    $('main').fadeOut(500, () => {
+
+  setInterval(function () {
+    var randomIndex = Math.floor((Math.random() * quotesList.length));
+
+    $('main').fadeOut(500, function () {
       $('#quote').html(quotesList[randomIndex]);
-      $('main').fadeIn(500, () => {
+
+      $('main').fadeIn(500, function () {
         queue.push(quotesList.splice(randomIndex));
+
         if (queue.length > 5) {
           quotesList.push(queue.shift());
         }
