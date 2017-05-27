50f2c083bc8fb71dd604ca878f1cb8c213d1a6eb
Eric Christie <ericjchristie@gmail.com>
2017-05-26 22:58:02 -0400

    wrote some javascript stuff

--- js/wisdom.js
+++ js/wisdom.js
@@ -1,14 +1,27 @@
-var quotes;
+const quotes;
+let queue = []; // use push() and shift() to get a queue
+let quotesList = [];
 
-$( document ).ready( function () {
-    // Remember this is called asynchronously
-    $.getJSON( './js/quotes.json', function ( data ) {
-        quotes = data.quotes;
-        initQuote();
-    } );
-} );
+$(document).ready(() => {
+  // Remember this is called asynchronously
+  $.getJSON('./js/quotes.json', (data) => {
+    quotes = data.quotes;
+    initQuote(quotes);
+  });
+});
 
-function initQuote() {
-    var randomIndex = Math.floor((Math.random() * quotes.length));
-    $('#quote').html(quotes[randomIndex]);
+function initQuote(quotes) {
+  quotesList = quotes;
+  setInterval(() => {
+    let randomIndex = Math.floor((Math.random() * quotesList.length));
+    $('main').fadeOut(500, () => {
+      $('#quote').html(quotesList[randomIndex]);
+      $('main').fadeIn(500, () => {
+        queue.push(quotesList.splice(randomIndex));
+        if (queue.length > 5) {
+          quotesList.push(queue.shift());
+        }
+      });
+    });
+  }, 3000);
 }
